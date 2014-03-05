95650e0404011834b50d8ab7bff8d2ec08bff2ef
gruhl.alexander@gmail.com <alexander@alexander-P5QL-E.(none)>
2014-03-06 00:06:06 +0100

    with errors?

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main{
 
 	public static void main(String[] args){
-	System.out.println("Test");
+	System.out.println("Test")
 //Comment Hier it was edited 
 	System.out.println("Hallo");
 //Comments
